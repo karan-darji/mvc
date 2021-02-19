65080785a34ea28b0279241bc963f11638ae7745
Karan Darji <karan.darji@arsenaltech.com>
2021-02-19 15:27:51 +0530

    first commit

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'test');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'mysql');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://localhost/mvc');
+
+
+define('PAGINATE_LIMIT', '5');
